97c2d4e8c1a1790020d30fe09a2b4be330114bcc
0xCUB3 <skula@mit.edu>
2026-07-10 15:28:26 -0400

    Stop ExtendedCss listener accumulation

--- extension-src/content.js
+++ extension-src/content.js
@@ -4951,17 +4951,15 @@ function _toPrimitive(t, r) { if ("object" != typeof t || !t) return t; var e = 
     } // enable dynamically added elements handling
 
     context.isDomObserved = true;
+    const eventTracker = new EventTracker();
+    context.eventTracker = eventTracker;
     context.domMutationObserver = new natives.MutationObserver(mutations => {
       if (!mutations || mutations.length === 0) {
         return;
       }
-      const eventTracker = new EventTracker();
       if (eventTracker.isIgnoredEventType() && shouldIgnoreMutations(mutations)) {
         return;
-      } // save instance of EventTracker to context
-      // for removing its event listeners on disconnectDocument() while mainDisconnect()
-
-      context.eventTracker = eventTracker;
+      }
       context.scheduler.run();
     });
     context.domMutationObserver.observe(document, {
